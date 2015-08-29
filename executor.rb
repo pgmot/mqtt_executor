f6f011906fec431ca13e8a48171e77333f64b2a9
mot <mot.contact.address.pc@gmail.com>
2015-08-30 01:24:22 +0900

    parse json

--- executor.rb
+++ executor.rb
@@ -1,4 +1,5 @@
 require 'mqtt'
+require 'json'
 require_relative './command'
 
 host = ENV['MQTT_HOST']
@@ -8,6 +9,7 @@ password = ENV['MQTT_PASS']
 MQTT::Client.connect(:remote_host => host, :username => username, :password => password) do |c|
   c.get(username) do |topic, message|
     puts "#{topic}: #{message}"
-    Command.exec(message['data']['payload']['command'])
+    json = JSON.parse(message)
+    Command.exec(message['payload']['command'])
   end
 end
