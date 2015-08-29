c04719bf75a377aae3c93769d8ca60ec25e5bec2
mot <mot.contact.address.pc@gmail.com>
2015-08-30 01:19:46 +0900

    add debug message

--- executor.rb
+++ executor.rb
@@ -7,6 +7,7 @@ password = ENV['MQTT_PASS']
 
 MQTT::Client.connect(:remote_host => host, :username => username, :password => password) do |c|
   c.get(username) do |topic, message|
+    puts "#{topic}: #{message}"
     Command.exec(message['data']['payload']['command'])
   end
 end
